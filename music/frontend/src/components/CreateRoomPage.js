597df22829eeca790fcb95809c981a3e07499ec9
pohern <ohern_p@yahoo.com>
2021-02-23 07:46:24 -0600

    add functionality to buttons, and tested create room

--- music/frontend/src/components/CreateRoomPage.js
+++ music/frontend/src/components/CreateRoomPage.js
@@ -15,6 +15,39 @@ export default class CreateRoomPage extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      guestCanPause: true,
+      votesToSkip: this.defaultVotes,
+    };
+    this.handleVotesChange = this.handleVotesChange.bind(this);
+    this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+    this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+  }
+
+  handleVotesChange(e) {
+    this.setState({
+      votesToSkip: e.target.value,
+    });
+  }
+
+  handleGuestCanPauseChange(e) {
+    this.setState({
+      guestCanPause: e.target.value === "true" ? true : false,
+    });
+  }
+
+  handleRoomButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        votes_to_skip: this.state.votesToSkip,
+        guest_can_pause: this.state.guestCanPause,
+      }),
+    };
+    fetch("/api/create-room", requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   render() {
@@ -22,28 +55,32 @@ export default class CreateRoomPage extends Component {
       <Grid container spacing={1}>
         <Grid item xs={12} align='center'>
           <Typography component='h4' variant='h4'>
-            Create a Room
+            Create A Room
           </Typography>
         </Grid>
         <Grid item xs={12} align='center'>
           <FormControl component='fieldset'>
             <FormHelperText>
               <div align='center'>Guest Control of Playback State</div>
-              <RadioGroup row defaultValue='true'>
-                <FormControlLabel
-                  value='true'
-                  control={<Radio color='primary' />}
-                  label='Play/Pause'
-                  labelPlacement='bottom'
-                />
-                <FormControlLabel
-                  value='false'
-                  control={<Radio color='secondary' />}
-                  label='No Control'
-                  labelPlacement='bottom'
-                />
-              </RadioGroup>
             </FormHelperText>
+            <RadioGroup
+              row
+              defaultValue='true'
+              onChange={this.handleGuestCanPauseChange}
+            >
+              <FormControlLabel
+                value='true'
+                control={<Radio color='primary' />}
+                label='Play/Pause'
+                labelPlacement='bottom'
+              />
+              <FormControlLabel
+                value='false'
+                control={<Radio color='secondary' />}
+                label='No Control'
+                labelPlacement='bottom'
+              />
+            </RadioGroup>
           </FormControl>
         </Grid>
         <Grid item xs={12} align='center'>
@@ -51,8 +88,12 @@ export default class CreateRoomPage extends Component {
             <TextField
               required={true}
               type='number'
+              onChange={this.handleVotesChange}
               defaultValue={this.defaultVotes}
-              inputProps={{ min: 1, style: { textAlign: "center" } }}
+              inputProps={{
+                min: 1,
+                style: { textAlign: "center" },
+              }}
             />
             <FormHelperText>
               <div align='center'>Votes Required To Skip Song</div>
@@ -60,7 +101,11 @@ export default class CreateRoomPage extends Component {
           </FormControl>
         </Grid>
         <Grid item xs={12} align='center'>
-          <Button color='primary' variant='contained'>
+          <Button
+            color='primary'
+            variant='contained'
+            onClick={this.handleRoomButtonPressed}
+          >
             Create A Room
           </Button>
         </Grid>
