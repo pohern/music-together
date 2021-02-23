1c823ee3563084c9192975d24eee429baa655bf2
pohern <ohern_p@yahoo.com>
2021-02-23 06:40:45 -0600

    setup React frontend components

--- music/frontend/src/components/App.js
+++ music/frontend/src/components/App.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import HomePage from './HomePage'
+import CreateRoomPage from './CreateRoomPage'
+import RoomJoinPage from './RoomJoinPage'
+ 
 
 export default class App extends Component {
     constructor(props){
@@ -7,7 +11,11 @@ export default class App extends Component {
     }
 
     render(){
-        return <h1>Testing React Code</h1>
+        return (
+          <div>
+            <HomePage />
+          </div>
+        );
     }
 }
 
