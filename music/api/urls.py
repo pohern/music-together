c98547ff311893657942dcfa152fd3f46eb376e9
pohern <ohern_p@yahoo.com>
2021-02-23 09:40:41 -0600

    add ability to join rooms with cde

--- music/api/urls.py
+++ music/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
 
 urlpatterns = [
     path('room', RoomView.as_view(), name='apiView'),
     path('create-room', CreateRoomView.as_view(), name='create-room'),
     path('get-room', GetRoom.as_view(), name='get-room'),
+    path('join-room', JoinRoom.as_view(), name='join-room')
 ]
 
--- music/frontend/src/components/RoomJoinPage.js
+++ music/frontend/src/components/RoomJoinPage.js
@@ -12,14 +12,12 @@ export default class RoomJoinPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        roomCode: '',
-        error: ''
-    }
-    this.handleTextFieldChange = this.handleTextFieldChange.bind(this)
-    this.roomButtonPressed = this.roomButtonPressed.bind(this)
+      roomCode: "",
+      error: "",
+    };
+    this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+    this.roomButtonPressed = this.roomButtonPressed.bind(this);
   }
-
-
 
   render() {
     return (
@@ -41,7 +39,11 @@ export default class RoomJoinPage extends Component {
           />
         </Grid>
         <Grid item xs={12} align='center'>
-          <Button variant='contained' color='primary' onClick={this.roomButtonPressed}>
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={this.roomButtonPressed}
+          >
             Enter Room
           </Button>
         </Grid>
@@ -54,13 +56,28 @@ export default class RoomJoinPage extends Component {
     );
   }
 
-  handleTextFieldChange(e){
-      this.setState({
-          roomCode: e.target.value
-      })
+  handleTextFieldChange(e) {
+    this.setState({
+      roomCode: e.target.value,
+    });
   }
 
-  roomButtonPressed(){
-    
+  roomButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        code: this.state.roomCode,
+      }),
+    };
+    fetch("/api/join-room", requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          this.props.history.push(`/room/${this.state.roomCode}`);
+        } else {
+          this.setState({ error: "Room Not Found" });
+        }
+      })
+      .catch((error) => console.log(error));
   }
 }
