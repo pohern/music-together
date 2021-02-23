db4b850fe6c9dac2c239275cd18c7cdf17b9eca4
pohern <ohern_p@yahoo.com>
2021-02-23 09:52:55 -0600

    add styling to homepage

--- music/frontend/src/components/HomePage.js
+++ music/frontend/src/components/HomePage.js
@@ -9,17 +9,41 @@ import {
   Link,
   Redirect,
 } from "react-router-dom";
+import {Grid, Typography, Button, ButtonGroup} from '@material-ui/core'
 
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
   }
 
+  renderHomePage(){
+      return (
+        <Grid container spacing={3}>
+          <Grid item xs={12} align='center'>
+            <Typography variant='h3' compact='h3'>
+              Music Together
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align='center'>
+            <ButtonGroup disableElevation variant='contained' color='primary'>
+              <Button color='primary' to='/join' component={Link}>
+                Join A Room
+              </Button>
+              <Button color='secondary' to='/create' component={Link}>
+                Create A Room
+              </Button>
+            </ButtonGroup>
+          </Grid>
+        </Grid>
+      );
+  }
+
+
   render() {
     return (
     <Router>
         <Switch>
-            <Route exact path='/'><p>This is the home page</p></Route>
+            <Route exact path='/'>{this.renderHomePage}</Route>
             <Route path='/join' component={RoomJoinPage}></Route>
             <Route path='/create' component={CreateRoomPage}></Route>
             <Route path='/room/:roomCode' component={Room}></Route>
