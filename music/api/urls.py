2681831d98e68b240e16bfac79ffe1e279be8ea4
pohern <ohern_p@yahoo.com>
2021-02-23 07:27:23 -0600

    add styling and layout to CreateRoomPage

--- music/api/urls.py
+++ music/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 
 urlpatterns = [
     path('room', RoomView.as_view(), name='apiView'),
+    path('create-room', CreateRoomView.as_view(), name='create-room')
 ]
 
