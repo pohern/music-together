01fc32069109117a0a8e793d590098fce76197c9
pohern <ohern_p@yahoo.com>
2021-02-23 08:54:50 -0600

    add Room component, and connect to backend

--- music/api/urls.py
+++ music/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView
+from .views import RoomView, CreateRoomView, GetRoom
 
 urlpatterns = [
     path('room', RoomView.as_view(), name='apiView'),
-    path('create-room', CreateRoomView.as_view(), name='create-room')
+    path('create-room', CreateRoomView.as_view(), name='create-room'),
+    path('get-room', GetRoom.as_view(), name='get-room'),
 ]
 
--- music/frontend/src/components/HomePage.js
+++ music/frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CreateRoomPage from "./CreateRoomPage";
 import RoomJoinPage from "./RoomJoinPage";
+import Room from './Room'
 import {
   BrowserRouter as Router,
   Switch,
@@ -21,6 +22,7 @@ export default class HomePage extends Component {
             <Route exact path='/'><p>This is the home page</p></Route>
             <Route path='/join' component={RoomJoinPage}></Route>
             <Route path='/create' component={CreateRoomPage}></Route>
+            <Route path='/room/:roomCode' component={Room}></Route>
         </Switch>
     </Router>
     )
