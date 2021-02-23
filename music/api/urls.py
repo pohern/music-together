e7083c8a1067e340f02348984ccbb089a42632d2
pohern <ohern_p@yahoo.com>
2021-02-23 11:58:30 -0600

    add function to bacend to update a room

--- music/api/urls.py
+++ music/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom, UpdateRoom
 
 urlpatterns = [
     path('room', RoomView.as_view(), name='apiView'),
@@ -7,6 +7,7 @@ urlpatterns = [
     path('get-room', GetRoom.as_view(), name='get-room'),
     path('join-room', JoinRoom.as_view(), name='join-room'),
     path('user-in-room', UserInRoom.as_view(), name='user-in-room'),
-    path('leave-room', LeaveRoom.as_view(), name='leave-room')
+    path('leave-room', LeaveRoom.as_view(), name='leave-room'),
+    path('update-room', UpdateRoom.as_view(), name='update-room')
 ]
 
