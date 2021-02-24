8edd4a541f3d606f5dea591b53e3274807cc0ab5
pohern <ohern_p@yahoo.com>
2021-02-23 19:54:55 -0600

    add funtion to refresh token from spotify

--- music/spotify/urls.py
+++ music/spotify/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AuthURL
+from .views import AuthURL, spotify_callback
 
 
 urlpatterns = [
     path('get-auth-url', AuthURL.as_view(), name='getAuth'),
+    path('redirect', spotify_callback)
     
 ]
--- music/spotify/views.py
+++ music/spotify/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import environ
 environ.Env()
 environ.Env.read_env()
@@ -10,6 +10,7 @@ from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
+from .util import update_or_create_user_tokens
 # Create your views here.
 
 class AuthURL(APIView):
@@ -42,3 +43,9 @@ def spotify_callback(request, format=None):
     refresh_token = response.get('refresh_token')
     expires_in = response.get('expires_in')
     error = response.get('error')
+
+    if not request.session.exists(request.session.session_key):
+            request.session.create()
+    update_or_create_user_tokens(request.session.session_key, access_token, token_type, expires_in, refresh_token)
+
+    return redirect('frontend:')
