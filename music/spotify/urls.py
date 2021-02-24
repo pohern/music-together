ea0d60e487e73110440f2cfca6ceceb64ea78e58
pohern <ohern_p@yahoo.com>
2021-02-23 19:59:09 -0600

    finsih backend code for spoyify

--- music/spotify/urls.py
+++ music/spotify/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AuthURL, spotify_callback
+from .views import AuthURL, spotify_callback, IsAuthenticated
 
 
 urlpatterns = [
     path('get-auth-url', AuthURL.as_view(), name='getAuth'),
     path('redirect', spotify_callback)
+    path('is-authenticated', IsAuthenticated.as_view())
     
 ]
--- music/spotify/views.py
+++ music/spotify/views.py
@@ -10,7 +10,7 @@ from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-from .util import update_or_create_user_tokens
+from .util import update_or_create_user_tokens, is_spotify_authenticated
 # Create your views here.
 
 class AuthURL(APIView):
@@ -49,3 +49,8 @@ def spotify_callback(request, format=None):
     update_or_create_user_tokens(request.session.session_key, access_token, token_type, expires_in, refresh_token)
 
     return redirect('frontend:')
+
+class IsAuthenticated(APIView):
+    def get(self, request, format=None):
+        is_authenticated = is_spotify_authenticated(self.request.session.session_key)
+        return Response({'status': 'Is Authenticated'}, status=status.HTTP_200_OK)
