e7dbc05049263c3beb4b1b9986ac15f4a3ec9e3a
pohern <ohern_p@yahoo.com>
2021-02-24 10:42:19 -0600

    add functionality to vote to skip songs in spotify views

--- music/spotify/views.py
+++ music/spotify/views.py
@@ -12,6 +12,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from .util import *
 from api.models import Room
+from .models import Vote
 # Create your views here.
 
 class AuthURL(APIView):
@@ -102,7 +103,17 @@ class CurrentSong(APIView):
             'id': song_id
         }
 
+        self.update_room_song(room, song_id)
+
         return Response(song, status=status.HTTP_200_OK)
+
+    def update_room_song(self, room, song_id):
+        current_song = room.current_song
+
+        if current_song != song_id:
+            room.current_song = song_id
+            room.save(update_fields=['current_song'])
+            votes = Vote.objects.filter(room=room).delete()
 
 class PauseSong(APIView):
     def put(self, response, format=None):
@@ -128,11 +139,15 @@ class SkipSong(APIView):
     def post(self, response, format=None):
         room_code = self.request.session.get('room_code')
         room = Room.objects.filter(code=room_code)[0]
+        votes = Vote.objects.filter(room=room, song_id=room.current_song)
+        votes_needed = room.votes_to_skip
 
-        if self.request.session.session_key == room.host:
-            skip_song(session_id)
+        if self.request.session.session_key == room.host or len(votes) + 1 >= votes_needed:
+            votes.delete()
+            skip_song(room.host)
 
         else: 
-            pass
+            vote = Vote(user=self.request.session.session_key, room=room, song_id=room.current_song)
+            vote.save()
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
