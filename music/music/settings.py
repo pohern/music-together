6b83a728106502863a82f62a1cb1d7a875c1e02b
pohern <ohern_p@yahoo.com>
2021-02-23 19:30:48 -0600

    add model for spotify tokens

--- music/music/settings.py
+++ music/music/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'spotify.apps.SpotifyConfig',
     'frontend.apps.FrontendConfig',
     'api.apps.ApiConfig',
     'rest_framework',
