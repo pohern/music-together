df36903a05491adb8f8fede36401c70f4eb7931e
pohern <ohern_p@yahoo.com>
2021-02-23 20:44:58 -0600

    add function to view currently playing song from spotify

--- music/spotify/urls.py
+++ music/spotify/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import AuthURL, spotify_callback, IsAuthenticated
+from .views import *
 
 
 urlpatterns = [
     path('get-auth-url', AuthURL.as_view(), name='getAuth'),
     path('redirect', spotify_callback),
     path('is-authenticated', IsAuthenticated.as_view()),
-    
+    path('current-song', CurrentSong.as_view())
 ]
--- music/spotify/util.py
+++ music/spotify/util.py
@@ -1,13 +1,15 @@
 from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta
-from requests import post
+from requests import post, put, get
 import environ
 environ.Env()
 environ.Env.read_env()
 import os
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
+
+BASE_URL = "https://api.spotify.com/v1/me/"
 
 def get_user_tokens(session_id):
     user_tokens = SpotifyToken.objects.filter(user=session_id)
@@ -40,7 +42,7 @@ def is_spotify_authenticated(session_id):
         
         return True
 
-        
+
     return False
 
 def refresh_spotify_token(session_id):
@@ -59,3 +61,18 @@ def refresh_spotify_token(session_id):
     refresh_token = response.get('refresh_token')
 
     update_or_create_user_tokens(session_id, access_token, token_type, expires_in, refresh_token)
+
+def execute_spotify_api_request(session_id, endpoint, post_=False, put_=False):
+    tokens = get_user_tokens(session_id)
+    headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + tokens.access_token}
+
+    if post_:
+        post(BASE_URL + endpoint, headers=headers)
+    if put_:
+        put(BASE_URL + endpoint, headers=headers)
+    
+    response = get(BASE_URL + endpoint, {}, headers=headers)
+    try:
+        return response.json()
+    except:
+        return {'Error': 'Issue With Request'}
